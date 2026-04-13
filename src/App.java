c5561b4529c9a411715e586fcc784a9477527f19
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-12 19:15:31 -0700

    finished wave 3

--- src/App.java
+++ src/App.java
@@ -45,5 +45,9 @@ public class App {
     // You should create a TruffulaOptions object using the args and
     // pass it to a new TruffulaPrinter that uses System.out
     // Then, call printTree on the TruffulaPrinter
+
+    TruffulaOptions options = new TruffulaOptions(args);
+    TruffulaPrinter printer = new TruffulaPrinter(options);
+    printer.printTree();
   }
 }
